1da669c1c8013e98efc63c5b06d06406b5c7d22b
radiolok <radiolok@yandex.ru>
2015-03-02 23:32:56 +0300

    MOD: Rewrite Buttons to Class and Add Hw interface for future use

---
+++ menuos/hardware.cpp
@@ -0,0 +1,43 @@
+/*Copyright (c) 2013, Artem Kashkanov
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+/***
+ *  This file define any links to hardware API or drivers.  
+ *  
+ */
+ 
+ 
+ static const uint8_t BSLOTS = 5; //buttons quantity
+ 
+//output registers definition:
+static const uint8_t BUTTONSPIN = PING ;
+static const uint8_t BUTTONSPORT = PORTG;
+static const uint8_t BUTTONSDDR = DDRG;
+static const uint8_t BUTTONSMASK = 0x1F; 
+ 
+ uint8_t HwButtonsCount(void){
+	return BSLOTS;
+ }
+ 
+ uint8_t HwButtonsSetup(uint8_t mode){
+	#if defined (RLN_ELECTRO_2012_SE)
+		BUTTONSDDR&=~BUTTONSMASK;//input
+		BUTTONSPORT=BUTTONSMASK;//setup buttons port pull-up
+	#else
+		error("undefined board");
+	#endif
+	return BUTTONSMASK;
+ }
+ 
+ uint8_t HwButtonsState(uint8_t mode){
+	//inverted, because pressed button return zero.
+	return(~BUTTONSPIN)&BUTTONSMASK;
+ }
---
+++ menuos/hardware.h
@@ -0,0 +1,69 @@
+/*Copyright (c) 2013, Artem Kashkanov
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+/***
+ *  This file define any links to hardware API or drivers.
+ *  This file is used for RLN electro buttons and text LCD definition 
+ *  
+ */
+ #ifndef HARDWARE_H_
+ #define HARDWARE_H_
+ 
+ #define RLN_ELECTRO_2012_SE
+ 
+ /**Button mask*/
+ enum{
+	BUTTONRETURN = 0x01,
+	BUTTONLEFT = 0x02,
+	BUTTONRIGHT = 0x10,
+	BUTTONSUP = 0x04,
+	BUTTONDOWN = 0x08,
+	BUTTONENTER = 0x10
+ }
+ 
+ /**button slot number
+  *  NOTICE: RIGHT and ENTER hase the same slot by default! MenuOS do not use ENTER button!
+  */
+ enum{
+	BUTTONRETURNSLOT = 0,
+	BUTTONLEFTSLOT = 1,
+	BUTTONRIGHTSLOT = 2,
+	BUTTONSUPSLOT = 3,
+	BUTTONDOWNSLOT = 4,
+	BUTTONENTERSLOT = 2	
+ }
+ 
+ uint8_t HwSlotsCount();  
+ 
+ /**
+ *  \brief Configure buttons port
+ *  
+ *  \param [in] mode not used
+ *  \return buttons mask
+ *  
+ *  \details Details
+ */
+uint8_t HwButtonsSetup(uint8_t mode);
+
+
+/**
+ *  \brief Return pressed buttons mask
+ *  
+ *  \param [in] mode not used
+ *  \return current buttons mask
+ *  
+ *  \details Details
+ */
+uint8_t HwButtonsState(uint8_t mode); 
+ 
+ 
+ #endif
+ 
